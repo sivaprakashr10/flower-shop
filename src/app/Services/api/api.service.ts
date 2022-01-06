84974fda3d58f34abf9d479e952972706d328e84
sivaprakashr10 <88925453+sivaprakashr10@users.noreply.github.com>
2022-01-06 17:25:07 +0530

    Initial commit

---
+++ src/app/Services/api/api.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ApiService {
+  banners = [
+    { banner: 'assets/imgs/1.jpg' },
+    { banner: 'assets/imgs/2.jpg' },
+    { banner: 'assets/imgs/3.jpg' },
+  ];
+
+  constructor() {}
+}
--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { ApiService } from './../Services/api/api.service';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
-
-  constructor() {}
-
+  banner: any;
+  cardOpts = {
+    initialSlide: 0,
+    speed: 400,
+    width: 120,
+  };
+  slideOpts = {
+    initialSlide: 0,
+    speed: 400,
+    autoplay: true,
+    loop: true,
+  };
+  constructor(private api: ApiService) {}
+  ngOnInit() {
+    this.banner = this.api.banners;
+  }
 }
